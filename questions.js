d7c359e43badd6b08f5625b5d318dfd412221f6e
Oaklee Ohmie <oakleeohmie@outlook.com>
2019-12-09 10:23:34 -0700

    displayed questions I think

--- questions.js
+++ questions.js
@@ -1,31 +1,76 @@
 var startButton = document.querySelector("#start");
+
 function Quiz(questions) {
     this.score = 0;
     this.questions = questions;
     this.questionIndex = 0;
 }
-var questions = [
-    {
-        title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
-    },
-    {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
-    },
-    {
-        title: "In JavaScript, what is a block of code called that is used to perform a specific task?",
-        choices: ["string", "variable", "function", "declaration"],
-        answer: "function"
+Quiz.prototype.getQuestionIndex = function () {
+    return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.guess = function (answer) {
+    if (this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
     }
-];
-function Question(title, choices, answer) {
-    this.title = title;
+
+    this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function () {
+    return this.questionIndex === this.questions.length;
+}
+
+function Question(text, choices, answer) {
+    this.text = text;
     this.choices = choices;
     this.answer = answer;
 }
+Question.prototype.isCorrectAnswer = function (choice) {
+    return this.answer === choice;
+}
+function populate() {
+    if (quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for (var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function () {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
 function startTimer() {
     function startTimer() {
         setTime();
@@ -46,8 +91,12 @@ function startTimer() {
         }
     }, 1000);
 }
+var questions = [
+    new Question("Commonly used data types DO NOT include:", ["strings", "booleans", "alerts", "numbers"], "alerts"),
+    new Question("The condition in an if / else statement is enclosed within ____.", ["quotes", "curly brackets", "parentheses", "square brackets"], "parentheses"),
+    new Question("In JavaScript, what is a block of code called that is used to perform a specific task?", ["string", "variable", "function", "declaration"], "function")
+];
+var quiz = new Quiz(questions);
 startButton.addEventListener("click", startTimer);
+populate();
 
-for (i = 0; i < questions.length; i++) {
-    textContent = (questions[index]);
-}
